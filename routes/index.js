c31dd8828610591e6339789b27519cd2f93ac89a
Carlos Rodriguez <rodriguezcarlos.cdr@gmail.com>
2024-02-10 14:20:59 -0300

    se cambio el nombre del archivo en ROUTES para definir un estandar

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const programa_mantenimiento = require('./endpointPrueba');
+const programa_mantenimiento = require('./prog_mantenimiento.js');
 
 function rutasApi(app){
   app.use('/programa_mantenimiento', programa_mantenimiento);
--- routes/prog_mantenimiento.router.js
+++ routes/prog_mantenimiento.router.js
@@ -5,7 +5,28 @@ const programa_mantenimiento = express.Router();
 programa_mantenimiento.get('/tareas', (req, res)=>{
   let tareas_programadas = [];
 
-  for (let index = 0; index < 20; index++) {
+  for (let index = 0; index < 10; index++) {
+    tareas_programadas.push({
+      maquina: faker.airline.airplane(),
+      tipo_tarea: 'reparar',
+      componente: faker.commerce.productName(),
+      criticidad: faker.number.int({min:0,max:10}),
+      frecuencia: 'mensual',
+      duracion_aprox: faker.number.float({min:1, max:100}),
+      fecha_ultima_int: faker.date.anytime(),
+      fecha_prox_int: faker.date.anytime(),
+      referencia_manual: faker.string.alphanumeric(),
+    });
+  }
+
+  res.json(tareas_programadas);
+})
+
+programa_mantenimiento.get('/tareas/:id_maq', (req, res)=>{
+  const {id_maq} = req.params;
+  let tareas_programadas = [];
+
+  for (let index = 0; index < id_maq; index++) {
     tareas_programadas.push({
       maquina: faker.airline.airplane(),
       tipo_tarea: 'reparar',
